29cdcc45dc2a424cda37f83da5d0a552ca82528d
Kevin Stradtman <stradtkt22@gmail.com>
2021-04-30 22:51:42 -0400

    Added capturing groups

---
+++ regex/advanced_regex/capturing_groups.py
@@ -0,0 +1,21 @@
+import re
+
+# groups are in parenthesis
+
+result = re.search(r"^(\w*), (\w*)$", "Lovelace, Ada")
+print(result)
+print(result.groups())
+print(result[0])
+print(result[1])
+print(result[2])
+
+
+def rearrange_name(name):
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+rearrange_name("Lovelace, Ada")
+rearrange_name("Richie, Dennis")
+rearrange_name("Hopper, Grace M.")
