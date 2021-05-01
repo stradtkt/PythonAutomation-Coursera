aecb91fe7f21d7383b5309e9591f273a5ae978ea
Kevin Stradtman <stradtkt22@gmail.com>
2021-04-30 20:55:12 -0400

    Added repetition qualifiers

---
+++ regex/repetition_qualifiers.py
@@ -0,0 +1,10 @@
+import re
+
+print(re.search(r"Py.*n", "Pygmalion"))
+print(re.search(r"Py.*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Python Programming"))
+print(re.search(r"o+l+", "goldfish"))
+print(re.search(r"o+l+", "woolly"))
+print(re.search(r"o+l+", "boil"))
+print(re.search(r"p?each", "To each their own"))
+print(re.search(r"p?each", "I like peaches"))
