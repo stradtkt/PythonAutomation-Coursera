21b500f85a66802d937f3f7c799c1a43e5bb7f50
Kevin Stradtman <stradtkt22@gmail.com>
2021-04-30 21:32:25 -0400

    Added app.py as a general file

---
+++ regex/app.py
@@ -0,0 +1,12 @@
+import re
+
+print(re.search(r"A.*a", "Argentina"))
+print(re.search(r"A.*a", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Azerbaijan"))
+print(re.search(r"^A.*a$", "Australia"))
+
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern, "this_is_a_valid_variable_name"))
+print(re.search(pattern, "this isn't a valid variable name"))
+print(re.search(pattern, "my_variable1"))
+print(re.search(pattern, "2my_variable1"))
